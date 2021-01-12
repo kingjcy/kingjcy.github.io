6a23b13ff38d0ce28562015caf423bfa17887d1d
kingjcy <chunyinjiang@MacBook-Pro-2.local>
2021-01-12 11:52:25 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+
+echo "Deploying myblog to github"
+
+# generate static html, css file using hugo
 cd ..
 hugo
+
+
 cd docs
 git add .
+
+if [ "$1" == "" ]; then
 git commit -m "update"
+else
+git commit -m "$1"
+fi
+
+# push to github
 git push -u origin master
